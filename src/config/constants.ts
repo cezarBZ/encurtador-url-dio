af1acd80f09b3dc7cbaf261672d8c22480490b66
cezarBZ <cezar30santos@gmail.com>
2022-01-14 10:35:53 -0300

    first commit

---
+++ src/config/constants.ts
@@ -0,0 +1,4 @@
+export const config = {
+    API_URL: 'http://localhost:5000',
+    MONGO_CONNECTION:'mongodb+srv://cezarBZ:cezar2002@cluster0.r2z9s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
